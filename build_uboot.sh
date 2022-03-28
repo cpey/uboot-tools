1a18d209628c9ae909bc20dee686c6044f54f978
Carles Pey <carles.pey@gmail.com>
2022-03-28 17:08:32 +0200

    Add config option to U-Boot build script

--- build_uboot.sh
+++ build_uboot.sh
@@ -6,15 +6,20 @@ ROOT_DIR=`pwd`
 BUILD_DIR=build
 UBOOT_DIR=u-boot
 VBOOT_DIR=${ROOT_DIR}/verified-boot/out2
+CONFIG=vexpress_ca9x4_defconfig
 
 SECURE_BOOT=0
 MENUCONFIG=0
 CLEAN_BUILD=0
-QEMU_CONFIG=0
 while [[ $# -gt 0 ]]; do
     key="$1"
     case $key in
-        -c|--clean)
+       -d|--defconfig)
+            CONFIG=$2
+            shift
+            shift
+            ;;
+       -c|--clean)
             CLEAN_BUILD=1
             shift
             ;;
@@ -24,10 +29,6 @@ while [[ $# -gt 0 ]]; do
             ;;
         -m|--menuconfig)
             MENUCONFIG=1
-            shift
-            ;;
-        -q|--qemu)
-            QEMU_CONFIG=1
             shift
             ;;
         *)
@@ -41,20 +42,19 @@ pushd `pwd`
 cd ${UBOOT_DIR}
 
 if [[ ${SECURE_BOOT} -eq 1 ]]; then
-	make O=${BUILD_DIR} ARCH=arm CROSS_COMPILE=${CC} EXT_DTB=${VBOOT_DIR}/vexpress-v2p-ca9-pubkey.dtb -j`nproc`
-	exit
+    make O=${BUILD_DIR} ARCH=arm CROSS_COMPILE=${CC} EXT_DTB=${VBOOT_DIR}/vexpress-v2p-ca9-pubkey.dtb -j`nproc`
+    popd
+    exit 0
 fi
 
 if [[ ${CLEAN_BUILD} -eq 1 ]]; then
     make O=${BUILD_DIR} ARCH=arm CROSS_COMPILE=${CC} distclean
-    if [[ ${QEMU_CONFIG} -eq 1 ]]; then
-        make O=${BUILD_DIR} ARCH=arm CROSS_COMPILE=${CC} qemu_arm_defconfig
-    else
-        make O=${BUILD_DIR} ARCH=arm CROSS_COMPILE=${CC} vexpress_ca9x4_defconfig
-    fi
+    make O=${BUILD_DIR} ARCH=arm CROSS_COMPILE=${CC} ${CONFIG}
 fi
+
 if [[ ${MENUCONFIG} -eq 1 ]]; then
     make O=${BUILD_DIR} ARCH=arm CROSS_COMPILE=${CC} menuconfig
+    make O=${BUILD_DIR} ARCH=arm CROSS_COMPILE=${CC} savedefconfig
 fi
 
 make O=${BUILD_DIR} ARCH=arm CROSS_COMPILE=${CC} -j`nproc`
