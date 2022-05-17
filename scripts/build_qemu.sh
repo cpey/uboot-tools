6bc63777014628cec6f7d3d30e6e8d0148cbdb74
Carles Pey <carles.pey@gmail.com>
2022-05-17 16:56:14 +0200

    Add -save-temps option to qemu build script

--- scripts/build_qemu.sh
+++ scripts/build_qemu.sh
@@ -5,13 +5,31 @@ set -ex
 dir=$(dirname $0)
 source ${dir}/config.sh
 
+while [[ $# -gt 0 ]]; do
+	key="$1"
+	case $key in
+		-s|--save-temps)
+			SAVE_TEMPS=1
+			shift
+			;;
+		*)
+			echo "Invalid argument"
+			exit 1
+			;;
+	esac
+done
+
 pushd `pwd`
 cd ${QEMU}
 
 [[ -d build ]] && rm -r build
 mkdir build && cd build
 
-../configure
+flags=""
+if [[ -n ${SAVE_TEMPS} ]]; then
+	flags="-save-temps"
+fi
+../configure --extra-cflags=${flags}
 make -j`nproc`
 
 popd
